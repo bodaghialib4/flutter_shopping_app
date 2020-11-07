4989c143b60bc4a2485e1d0c412d93012c691a4a
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-07 16:14:00 +0330

    implement contents of models

---
+++ lib/models/category_model.dart
@@ -0,0 +1,8 @@
+
+class CategoryModel{
+  String categoryName;
+  String imgAssetPath;
+  String color1;
+  String color2;
+  CategoryModel({this.categoryName, this.imgAssetPath, this.color1, this.color2});
+}
---
+++ lib/models/trending_product_model.dart
@@ -0,0 +1,11 @@
+
+class TrendingProductModel{
+  String productName;
+  String storeName;
+  String imgUrl;
+  int priceInDollars;
+  int noOfRating;
+  int rating;
+  TrendingProductModel({this.productName, this.storeName, this.imgUrl,
+  this.priceInDollars, this.noOfRating, this.rating});
+}
