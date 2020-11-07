b98670cf1787c2474d11124b2caeb3826802acd7
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-07 17:42:48 +0330

    implement initState of _MyHomePageState (defining trendingProducts & products & categories)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_app/data/data.dart';
+import 'package:flutter_shopping_app/models/category_model.dart';
+import 'package:flutter_shopping_app/models/product_model.dart';
+import 'package:flutter_shopping_app/models/trending_product_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,6 +29,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  List<TrendingProductModel> trendingProducts = new List<TrendingProductModel>();
+  List<ProductModel> products = new List<ProductModel>();
+  List<CategoryModel> categories = new List<CategoryModel>();
+  @override
+  void initState() {
+    super.initState();
+    trendingProducts = getTrendingProducts();
+    products = getProduct();
+    categories = getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
