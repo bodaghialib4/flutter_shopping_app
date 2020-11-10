7ff74aa9226a03259742ca4fa290888aa27a1bcb
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-10 23:08:25 +0330

    adding Search Bar in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,37 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 50,
             ),
             //here we will add our container
+
+            /// Search Bar
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 12),
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      offset: Offset(5.0, 5.0),
+                      blurRadius: 5.0,
+                      color: Colors.black87.withOpacity(0.05),
+                    )
+                  ]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 9),
+                    child: Text(
+                      "Search",
+                      style: TextStyle(color: Color(0xff9B9B9B), fontSize: 17),
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(Icons.search),
+                ],
+              ),
+            ),
+
           ],
         ),
       ),
