44092f6226301127573e74f9668c6347d45a056c
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-10 23:07:17 +0330

    complete implementation of CategoryTile in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -291,9 +291,42 @@ class ProductTile extends StatelessWidget {
 }
 
 class CategoryTile extends StatelessWidget {
+  final String categoryName;
+  final String imgAssetPath;
+  final String color1;
+  final String color2;
+
+  CategoryTile(
+      {this.categoryName, this.imgAssetPath, this.color1, this.color2});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 65,
+            width: 110,
+            margin: EdgeInsets.only(right: 16),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(int.parse(color1)), Color(int.parse(color2))],
+                ),
+                borderRadius: BorderRadius.circular(8)),
+            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 8),
+            child: Container(
+              child: Image.asset(
+                imgAssetPath,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(categoryName),
+        ],
+      ),
+    );
   }
 }
