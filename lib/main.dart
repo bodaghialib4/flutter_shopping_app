7b6bcab444fd2980839076e14002fb62034250d8
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-10 23:04:29 +0330

    change a misspelling (title->tile) and some core reforming

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  List<TrendingProductModel> trendingProducts = new List<TrendingProductModel>();
+  List<TrendingProductModel> trendingProducts =
+      new List<TrendingProductModel>();
   List<ProductModel> products = new List<ProductModel>();
   List<CategoryModel> categories = new List<CategoryModel>();
 
@@ -48,10 +48,11 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
             //here we will add our container
           ],
         ),
@@ -60,27 +61,33 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class TrendingTitle extends StatelessWidget{
+class TrendingTile extends StatelessWidget {
   final String productName;
   final String storeName;
   final String imgUrl;
   final int noOfRating;
   final int rating;
   final int priceInDollars;
-  TrendingTitle({this.productName, this.storeName, this.imgUrl, this.noOfRating,
-    this.rating, this.priceInDollars});
+
+  TrendingTile(
+      {this.productName,
+      this.storeName,
+      this.imgUrl,
+      this.noOfRating,
+      this.rating,
+      this.priceInDollars});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width - 70,
+      width: 300,
       margin: EdgeInsets.only(right: 13),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(4),
         boxShadow: <BoxShadow>[
           BoxShadow(
-            offset: Offset(1.0,1.0),
+            offset: Offset(1.0, 1.0),
             blurRadius: 15.0,
             color: Colors.black87.withOpacity(0.05),
           ),
@@ -91,18 +98,22 @@ class TrendingTitle extends StatelessWidget{
           Container(
             child: Stack(
               children: <Widget>[
-                Image.asset("assets/image.png", height: 150, fit: BoxFit.cover,),
+                Image.asset(
+                  "assets/image.png",
+                  height: 150,
+                  fit: BoxFit.cover,
+                ),
                 Container(
                   height: 25,
                   width: 50,
                   margin: EdgeInsets.only(left: 5, top: 5),
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                    gradient: LinearGradient(
-                      colors: [const Color(0xff8EA2FF).withOpacity(0.5),
-                        const Color(0xff557AC7).withOpacity(0.5)]
-                    ),
+                    borderRadius: BorderRadius.circular(6),
+                    gradient: LinearGradient(colors: [
+                      const Color(0xff8EA2FF).withOpacity(0.5),
+                      const Color(0xff557AC7).withOpacity(0.5)
+                    ]),
                   ),
                   child: Text(
                     "\$$priceInDollars",
@@ -119,33 +130,45 @@ class TrendingTitle extends StatelessWidget{
               children: <Widget>[
                 Text(
                   productName,
-                  style: TextStyle(color: Colors.black87, fontSize: 19, ),
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 19,
+                  ),
                 ),
                 Text(
                   storeName,
                   style: TextStyle(color: textGrey),
                 ),
-                SizedBox(height: 8,),
+                SizedBox(
+                  height: 8,
+                ),
                 Row(
                   children: <Widget>[
-                    StarRating(rating:rating,),
-                    SizedBox(width: 3,),
+                    StarRating(
+                      rating: rating,
+                    ),
+                    SizedBox(
+                      width: 3,
+                    ),
                     Text(
                       "($noOfRating)",
-                      style: TextStyle( color: textGrey, fontSize: 12),
+                      style: TextStyle(color: textGrey, fontSize: 12),
                     ),
                   ],
                 ),
-                SizedBox(height: 13,),
+                SizedBox(
+                  height: 13,
+                ),
                 Container(
                   height: 30,
                   padding: EdgeInsets.symmetric(horizontal: 12),
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(4),
-                    gradient: LinearGradient(
-                        colors: [const Color(0xff8EA2FF), const Color(0xff557AC7)]
-                    ),
+                    gradient: LinearGradient(colors: [
+                      const Color(0xff8EA2FF),
+                      const Color(0xff557AC7)
+                    ]),
                   ),
                   child: Text(
                     "Add to cart",
@@ -161,24 +184,46 @@ class TrendingTitle extends StatelessWidget{
   }
 }
 
-class StarRating extends StatelessWidget{
+class StarRating extends StatelessWidget {
   final int rating;
+
   StarRating({this.rating});
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        Image.asset(rating>=1?"assets/star.png":"assets/stargrey.png",width: 13, height: 13,),
-        Image.asset(rating>=2?"assets/star.png":"assets/stargrey.png",width: 13, height: 13,),
-        Image.asset(rating>=3?"assets/star.png":"assets/stargrey.png",width: 13, height: 13,),
-        Image.asset(rating>=4?"assets/star.png":"assets/stargrey.png",width: 13, height: 13,),
-        Image.asset(rating>=5?"assets/star.png":"assets/stargrey.png",width: 13, height: 13,),
+        Image.asset(
+          rating >= 1 ? "assets/star.png" : "assets/stargrey.png",
+          width: 13,
+          height: 13,
+        ),
+        Image.asset(
+          rating >= 2 ? "assets/star.png" : "assets/stargrey.png",
+          width: 13,
+          height: 13,
+        ),
+        Image.asset(
+          rating >= 3 ? "assets/star.png" : "assets/stargrey.png",
+          width: 13,
+          height: 13,
+        ),
+        Image.asset(
+          rating >= 4 ? "assets/star.png" : "assets/stargrey.png",
+          width: 13,
+          height: 13,
+        ),
+        Image.asset(
+          rating >= 5 ? "assets/star.png" : "assets/stargrey.png",
+          width: 13,
+          height: 13,
+        ),
       ],
     );
   }
 }
 
-class ProductTitle extends StatelessWidget{
+class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -186,7 +231,7 @@ class ProductTitle extends StatelessWidget{
   }
 }
 
-class CategoryTitle extends StatelessWidget{
+class CategoryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
